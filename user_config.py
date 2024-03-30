5095318e505b9a91fcc5fb86df4858748002d6e3
babutree <babutree@outlook.com>
2024-03-30 21:43:52 +0800

    Create user_config.py

---
+++ user_config.py
@@ -0,0 +1,27 @@
+source_file = "user_demo.txt"
+final_file = "user_result.txt"
+favorite_list = [
+    "CCTV-1",
+    "CCTV-2", 
+    "CCTV-3",  
+    "CCTV-5",
+    "CCTV-5+",
+    "CCTV-6",  
+    "CCTV-7",  
+    "CCTV-8",  
+    "CCTV-10",  
+    "CCTV-13",
+    "CCTV-14",  
+    "广东卫视",
+    "浙江卫视",
+    "湖南卫视",
+    "陕西卫视",  
+    "翡翠台",
+]
+favorite_page_num = 6
+default_page_num = 4
+urls_limit = 15
+response_time_weight = 0.5
+resolution_weight = 0.5
+recent_days = 30
+ipv_type = "ipv4"
